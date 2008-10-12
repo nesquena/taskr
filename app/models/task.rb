7f40ccae254e054880bf6d1e7a59e972a771ffbc
Nathan Esquenazi <nesquena@gmail.com>
2008-10-12 03:01:03 -0700

    Added task migration, added schema and annotated task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+# Schema version: 20081012094223
+#
+# Table name: tasks
+#
+#  id          :integer         not null, primary key
+#  description :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Task < ActiveRecord::Base
   
 end
